5ff95ecfd28190905575afd87b552025159337a5
qiuhaoxin <502967244@qq.com>
2018-03-22 00:19:31 +0800

    commit

--- config/env.js
+++ config/env.js
@@ -1,1 +1,16 @@
+const fs=require('fs');
+const path=require('path');
+const paths=require('./paths');
 
+delete require.cache[require.resolve('./paths')];
+
+const NODE_ENV=process.env.NODE_ENV;
+if(!NODE_ENV){
+	throw new Error(
+    "the NODE_ENV enviroment variable is required but was not specified";
+	);
+}
+
+var dotenvFiles=[
+   `${paths.dot}`
+]
--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -3,4 +3,24 @@
 */
 const path=require('path');
 const webpack=require('webpack');
-const 
+const autoprefixer=require('autoprefixer');
+const HtmlWebpackPlugin=require('htmo-webpack-plugin');
+const CaseSensitivePathsPlugin=require('case-sensitive-paths=webpack-plugin');
+const InterpolateHtmlPlugin=require('react-dev-utils/InterpolateHtmlPlugin');
+const WatchMissingNodeModulesPlugin=require('react-dev-utils/WatchMissingNodeModulesPlugin');
+const eslintFormatter=require('react-dev-utils/eslintFormatter');
+const ModuleScopePlugin=require('react-dev-utils/ModuleScopePlugin');
+const paths=require('./paths');
+const px2rem=require('postcss-px2rem');
+const getClientEnviroment=require('./env');
+
+
+function resolve(dir){
+	return path.join(__dirname,"..",dir);
+}
+
+const publicPath="/";
+
+const publicUrl="";
+
+const env=
