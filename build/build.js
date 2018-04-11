e7215b3266530f81b077fc63098c4cd3afdc6d3d
qiuhaoxin <502967244@qq.com>
2018-04-11 11:48:41 +0800

    commit

---
+++ build/build.js
@@ -0,0 +1,143 @@
+'use strict';
+
+process.env.NODE_ENV="production";
+process.env.BABEL_ENV="production";
+process.env.STATIC_ENV="production";
+
+if(process.env.STATIC_ENV=='production'){
+	process.env.PUBLIC_URL="/wisemobile";
+}
+
+process.on('unhandledRejection',err=>{
+	throw err;
+})
+
+require('../config/env');
+
+const path=require('path');
+const chalk=require('chalk');
+const fs=require('fs-extra');
+const webpack=require('webpack');
+const config=require('../config/webpack.config.prod');
+const paths=require('../config/paths');
+const checkRequiredFiles=require('react-dev-utils/checkRequiredFiles');
+const formatWebpackMessages=require('react-dev-utils/formatWebpackMessages');
+const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
+const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
+const printBuildError = require('react-dev-utils/printBuildError');
+
+
+const measureFileSizesBeforeBuild=
+      FileSizeReporter.measureFileSizesBeforeBuild;
+
+const printFileSizeAfterBuild=FileSizeReporter.printFileSizeAfterBuild;
+
+const useYarn=fs.existsSync(paths.yarnLockFile);
+
+const WARN_AFTER_BUILD_GZIP_SIZE=512 * 1024;
+const WARN_AFTER_CHUNK_GZIP_SIZE=1024 * 1024;
+
+
+if(!checkRequiredFiles([paths.appHtml,paths.appIndexJs])){
+	process.exit(1);
+}
+
+measureFileSizesBeforeBuild(paths.appBuild)
+  .then(previousFileSize=>{
+  	fs.emptyDirSync(paths.appBuild);
+  	copyPublicFolder();
+  	return build(previousFileSize);
+  })
+  .then(
+     ({stats,previousFileSize,warnings})=>{
+     	if(warnings.length){
+     		console.log(chalk.yellow('Compiled with warnings.\n'));
+     		console.log(warnings.join("\r\n"));
+     		console.log(
+     			'\n Search for the '+
+     			chalk.underline(chalk.yellow('keywords'))+
+     			' to learn more about each warning'
+     		);
+     		console.log(
+              'To ignore,and '+
+              chalk.cyan('// eslint-disable-next-line')+
+              'to the line before.\n'
+     		)
+     	}else{
+     		console.log(chalk.green('Compiled successfully.\n'));
+     	}
+     	console.log("File sizes after gzip:\n");
+     	printFileSizeAfterBuild(
+          stats,
+          previousFileSize,
+          path.appBuild,
+          WARN_AFTER_BUILD_GZIP_SIZE,
+          WARN_AFTER_CHUNK_GZIP_SIZE,
+     	)
+     	const appPackage=require(paths.appPackageJson);
+     	const publicUrl=paths.publicUrl;
+     	const publicPath=config.output.publicPath;
+     	const buildFolder=path.relative(process.cwd(),paths.appBuild);
+
+     	printHostingInstructions(
+           appPackage,
+           publicUrl,
+           publicPath,
+           buildFolder,
+           useYarn
+     	);
+     },
+     err=>{
+     	console.log(chalk.red('Failded to compile.\n'));
+     	printBuildError(err);
+     	process.exit(1);
+     }
+  );
+
+  function build(previousFileSizes){
+  console.log('Creating an optimized production build...');
+
+  let compiler = webpack(config);
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err) {
+        return reject(err);
+      }
+      const messages = formatWebpackMessages(stats.toJson({}, true));
+      if (messages.errors.length) {
+        // Only keep the first error. Others are often indicative
+        // of the same problem, but confuse the reader with noise.
+        if (messages.errors.length > 1) {
+          messages.errors.length = 1;
+        }
+        return reject(new Error(messages.errors.join('\n\n')));
+      }
+      if (
+        process.env.CI &&
+        (typeof process.env.CI !== 'string' ||
+          process.env.CI.toLowerCase() !== 'false') &&
+        messages.warnings.length
+      ) {
+        console.log(
+          chalk.yellow(
+            '\nTreating warnings as errors because process.env.CI = true.\n' +
+              'Most CI servers set it automatically.\n'
+          )
+        );
+        return reject(new Error(messages.warnings.join('\n\n')));
+      }
+      return resolve({
+        stats,
+        previousFileSizes,
+        warnings: messages.warnings,
+      });
+    });
+  });
+  }
+
+  function copyPublicFolder(){
+  	fs.copySync(paths.appPublic,paths.appBuild,{
+  		dereference:true,
+  		filter:file=>file!==paths.appHtml,
+  	});
+  }
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -3,7 +3,7 @@ const webpack=require('webpack');
 
 const HtmlWebpackPlugin=require('html-webpack-plugin');
 const ExtractTextPlugin=require('extract-text-webpack-plugin');
-const ManifestPlugin=require('webpack-maifest-plugin');
+const ManifestPlugin=require('webpack-manifest-plugin');
 
 const paths=require('./paths');
 const getClientEnvironment=require('./env');
@@ -24,9 +24,9 @@ function resolve(dir){
 	return path.join(__dirname,"..",dir)
 }
 
-if(env.stringified['process.env'].NODE_ENV!=='production'){
-	throw new Error("production build must have NODE_ENV=production");
-}
+// if(env.stringified['process.env'].NODE_ENV!=='production'){
+// 	throw new Error("production build must have NODE_ENV=production");
+// }
 const cssFileName="static/css/[name].[contenthash:8].css";
 
 const extractTextPluginOptions=shouldUseRelativeAssetPath ? {publicPath:Array(cssFileName.split('/').length).join('../')} : {};
@@ -34,10 +34,29 @@ const extractTextPluginOptions=shouldUseRelativeAssetPath ? {publicPath:Array(cs
 module.exports={
 	bail:true,
 	devtool:false,
-	entry:['babel-polyfill',require('./polyfill'),paths.appIndexJs],
+	entry:['babel-polyfill',require.resolve('./polyfills'),paths.appIndexJs],
 	output:{
-		path:paths.appBuild
-	}
+		path:paths.appBuild,
+		filename:'static/js/[name].[cunkhash:8].chunk.js',
+		publicPath:publicPath,
+		devtoolModuleFilenameTemplate:info=>
+		  path.relative(paths.appSrc,info.absoluteResourcePath)
+		  .replace(/\\/g,'/'),
+	},
+	resolve:{
+		modules:['node_modules',paths.appNodeModules].concat(
+              process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
+		),
+		extensions:['.js','.jsx','.less','.json','.css'],
+		alias:{
+			'babel-runtime':path.dirname(
+                require.resolve('babel-runtime/package.json')
+			)
+		}
+	},
+	plugins:[
+
+	]
 }
 
 
